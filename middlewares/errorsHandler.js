a4a009f99c2bd68391567dbb00ac8f0e052a7795
Orlov Alexander <orlov.03.05.2004@mail.ru>
2022-01-23 12:54:23 +0300

    fix: delete no-console rules eslint

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -1,4 +1,3 @@
 module.exports = (err, origin) => {
-  // eslint-disable-next-line no-console
   console.log(`${origin} ${err.name} c текстом ${err.message} не была обработана. Обратите внимание!`);
 };
